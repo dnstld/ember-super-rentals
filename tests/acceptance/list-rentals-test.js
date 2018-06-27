d767d0fa25e2a3d8afbe2fae5b526fc7a85a589e
Denis Toledo <contato@denistoledo.com.br>
2018-06-27 08:14:51 -0300

    helpers, component, mirage

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { click, visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | list rentals', function(hooks) {
@@ -11,12 +11,20 @@ module('Acceptance | list rentals', function(hooks) {
   });
 
   test('should link to information about the company.', async function (assert) {
+    await visit('/');
+    await click(".menu-about");
+    assert.equal(currentURL(), '/about', 'should navigate to about');
   });
 
   test('should link to contact information.', async function (assert) {
+    await visit('/');
+    await click(".menu-contact");
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
   });
 
   test('should list available rentals.', async function (assert) {
+    await visit('/');
+    assert.equal(this.element.querySelectorAll('.listing').length, 3, 'should display 3 listings');
   });
 
   test('should filter the list of rentals by city.', async function (assert) {
